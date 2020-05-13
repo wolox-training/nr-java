e697df1b29cd9a8182d4c0c49def5eb0e5fdabf1
Rodriguez <A127279@bf.arg.igrupobbva>
2020-05-13 15:45:39 -0300

    Correccion de errores multiples en Test

--- src/main/java/wolox/training/exceptions/BookNotFoundException.java
+++ src/main/java/wolox/training/exceptions/BookNotFoundException.java
@@ -1,15 +1,19 @@
 package wolox.training.exceptions;
 
-public class BookNotFoundException  extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Book not found")
+public class BookNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-    public BookNotFoundException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+	public BookNotFoundException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
 
-    public BookNotFoundException(final String message) {
-        super(message);
-    }    
+	public BookNotFoundException(final String message) {
+		super(message);
+	}
 
 }
--- src/main/java/wolox/training/exceptions/UserNotFoundException.java
+++ src/main/java/wolox/training/exceptions/UserNotFoundException.java
@@ -1,15 +1,19 @@
 package wolox.training.exceptions;
 
-public class UserNotFoundException  extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found")
+public class UserNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-    public UserNotFoundException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+	public UserNotFoundException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
 
-    public UserNotFoundException(final String message) {
-        super(message);
-    }    
+	public UserNotFoundException(final String message) {
+		super(message);
+	}
 
 }
